ea782afe83fe245daf7fcdd906eadd306fb2919f
Tun Huang <huang.tun.anton@gmail.com>
2022-12-28 20:43:10 +0100

    Finish day-09

--- day-09/index.js
+++ day-09/index.js
@@ -1,127 +1,138 @@
 const fs = require('fs');
 const input = fs.readFileSync('input.txt').toString();
-// const input = `R 4
-// U 4
-// L 3
-// D 1
-// R 4
-// D 1
-// L 5
-// R 2`;
 const inputArray = input
   .split('\n')
   .map((line) => line.split(' '))
   .map((motion) => [motion[0], +motion[1]]);
 // [y-coordinate, x-coordinate]
-const headPosition = [0, 0];
-const tailPosition = [0, 0];
-// console.log(inputArray);
-function moveHeadThenTail(head, tail, motion, visited) {
+// Part 1
+// const rope = [[0, 0], [0, 0]];
+// Part 2
+const rope = [
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+];
+
+function moveRope(rope, motion, visited) {
   switch (motion[0]) {
     case 'U':
       for (let i = 0; i < motion[1]; i++) {
-        head[0]--;
-        moveNextKnot(head, tail);
-        visited.add(createPositionString(tail));
+        rope[0][0]--;
+        for (let j = 0; j < rope.length - 1; j++) {
+          moveNextKnot(rope[j], rope[j + 1]);
+        }
+        visited.add(createPositionString(rope.at(-1)));
       }
       break;
     case 'D':
       for (let i = 0; i < motion[1]; i++) {
-        head[0]++;
-        moveNextKnot(head, tail);
-        visited.add(createPositionString(tail));
+        rope[0][0]++;
+        for (let j = 0; j < rope.length - 1; j++) {
+          moveNextKnot(rope[j], rope[j + 1]);
+        }
+        visited.add(createPositionString(rope.at(-1)));
       }
       break;
     case 'L':
       for (let i = 0; i < motion[1]; i++) {
-        head[1]--;
-        moveNextKnot(head, tail);
-        visited.add(createPositionString(tail));
+        rope[0][1]--;
+        for (let j = 0; j < rope.length - 1; j++) {
+          moveNextKnot(rope[j], rope[j + 1]);
+        }
+        visited.add(createPositionString(rope.at(-1)));
       }
       break;
     case 'R':
       for (let i = 0; i < motion[1]; i++) {
-        head[1]++;
-        moveNextKnot(head, tail);
-        visited.add(createPositionString(tail));
+        rope[0][1]++;
+        for (let j = 0; j < rope.length - 1; j++) {
+          moveNextKnot(rope[j], rope[j + 1]);
+        }
+        visited.add(createPositionString(rope.at(-1)));
       }
       break;
     default:
       throw new Error('Unknown motion for Head: ' + motion);
   }
-  // return visited;
 }
 
-function moveNextKnot(head, tail) {
+function moveNextKnot(currKnot, nextKnot) {
   // overlap
-  if (head[0] === tail[0] && head[1] === tail[1]) {
+  if (currKnot[0] === nextKnot[0] && currKnot[1] === nextKnot[1]) {
     return;
   }
   // diagonal exact by 1
-  if (Math.abs(head[0] - tail[0]) === 1 && Math.abs(head[1] - tail[1]) === 1) {
+  if (
+    Math.abs(currKnot[0] - nextKnot[0]) === 1 &&
+    Math.abs(currKnot[1] - nextKnot[1]) === 1
+  ) {
     return;
   }
   // same row
-  if (head[0] === tail[0]) {
-    if (Math.abs(head[1] - tail[1]) === 1) {
+  if (currKnot[0] === nextKnot[0]) {
+    if (Math.abs(currKnot[1] - nextKnot[1]) === 1) {
       return;
     }
-    if (head[1] > tail[1]) {
-      tail[1]++;
+    if (currKnot[1] > nextKnot[1]) {
+      nextKnot[1]++;
     } else {
-      tail[1]--;
+      nextKnot[1]--;
     }
   }
   // same column
-  if (head[1] === tail[1]) {
-    if (Math.abs(head[0] - tail[0]) === 1) {
+  if (currKnot[1] === nextKnot[1]) {
+    if (Math.abs(currKnot[0] - nextKnot[0]) === 1) {
       return;
     }
-    if (head[0] > tail[0]) {
-      tail[0]++;
+    if (currKnot[0] > nextKnot[0]) {
+      nextKnot[0]++;
     } else {
-      tail[0]--;
+      nextKnot[0]--;
     }
   }
   // bottom right
-  if (head[0] > tail[0] && head[1] > tail[1]) {
-    tail[0]++;
-    tail[1]++;
+  if (currKnot[0] > nextKnot[0] && currKnot[1] > nextKnot[1]) {
+    nextKnot[0]++;
+    nextKnot[1]++;
   }
   // bottom left
-  if (head[0] > tail[0] && head[1] < tail[1]) {
-    tail[0]++;
-    tail[1]--;
+  if (currKnot[0] > nextKnot[0] && currKnot[1] < nextKnot[1]) {
+    nextKnot[0]++;
+    nextKnot[1]--;
   }
   // top left
-  if (head[0] < tail[0] && head[1] < tail[1]) {
-    tail[0]--;
-    tail[1]--;
+  if (currKnot[0] < nextKnot[0] && currKnot[1] < nextKnot[1]) {
+    nextKnot[0]--;
+    nextKnot[1]--;
   }
   // top right
-  if (head[0] < tail[0] && head[1] > tail[1]) {
-    tail[0]--;
-    tail[1]++;
+  if (currKnot[0] < nextKnot[0] && currKnot[1] > nextKnot[1]) {
+    nextKnot[0]--;
+    nextKnot[1]++;
   }
 }
 
 const createPositionString = (positionArray) =>
   positionArray[0] + '-' + positionArray[1];
 
-function simulateMotions(head, tail, motions) {
+function simulateMotions(rope, motions) {
   const positionsVisitedByTail = new Set();
-  positionsVisitedByTail.add(createPositionString(tailPosition));
+  positionsVisitedByTail.add(createPositionString(rope.at(-1)));
 
   motions.forEach((motion) => {
-    moveHeadThenTail(head, tail, motion, positionsVisitedByTail);
+    moveRope(rope, motion, positionsVisitedByTail);
   });
 
   return positionsVisitedByTail;
 }
 
-const positionsVisitedByTail = simulateMotions(
-  headPosition,
-  tailPosition,
-  inputArray
-);
+const positionsVisitedByTail = simulateMotions(rope, inputArray);
 console.log(positionsVisitedByTail.size);
